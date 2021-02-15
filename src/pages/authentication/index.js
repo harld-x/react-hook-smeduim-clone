9b39a929386d46fd8b30ad1c0981f1a12185834a
harld-x <danilyudin723@gmail.com>
2021-02-15 22:08:52 +0700

    routes, usestate

---
+++ src/pages/authentication/index.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import {Link} from "react-router-dom";
+
+
+const Authentication = () => {
+    const [user, setUser] = useState({name: ''})
+
+    const handleCounter = () => {
+       setUser({name: 'Foo'})
+    }
+    console.log('user', user)
+
+    return (
+        <div className='auth-page'>
+            <button onClick={handleCounter}>Counter</button>
+          <div className="container page">
+              <div className="row">
+                  <div className="col-md-6 offset-md-3 col-xs-12">
+                      <h1 className='text-xs-center'>Login</h1>
+                      <p className='text-xs-center'>
+                          <Link to='register'>Need an account?</Link>
+                      </p>
+                      <form>
+                          <fieldset>
+                              <fieldset className="form-group">
+                                  <input
+                                      type='email'
+                                      className='form-control form-control-lg'
+                                      placeholder='Email'
+                                  />
+                              </fieldset>
+
+                              <fieldset className="form-group">
+                                  <input
+                                      type='password'
+                                      className='form-control form-control-lg'
+                                      placeholder='Password'
+                                  />
+                              </fieldset>
+
+                              <button className='btn btn-lg btn-primary pull-xs-right' type='submit'>
+                                  Sign In
+                              </button>
+
+                          </fieldset>
+                      </form>
+                  </div>
+              </div>
+          </div>
+        </div>
+    )
+}
+
+export default Authentication
