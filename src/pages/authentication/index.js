7597aac8ae8199c358c346bd96fe66d04ec20f91
harld-x <danilyudin723@gmail.com>
2021-02-16 21:24:37 +0700

    creating usefetch hook

--- src/pages/authentication/index.js
+++ src/pages/authentication/index.js
@@ -1,83 +1,73 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom'
 import axios from 'axios'
 
+import useFetch from "../../hooks/useFetch";
+
 const Authentication = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [isSubmitting, setIsSubmitting] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [{isLoading, error, response}, doFetch] = useFetch('/users/login')
+  console.log('useFetch', isLoading, error, response)
 
+  const handleSubmit = event => {
+    event.preventDefault()
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        console.log('data', email, password)
-        setIsSubmitting(true)
-    }
-
-    useEffect(() => {
-        if(!isSubmitting) {
-            return
+    doFetch({
+      method: 'post',
+      data: {
+        user: {
+          email: 'qq@qq.com',
+          password: '123'
         }
-        axios('https://conduit.productionready.io/api/users/login', {
-            method: 'post',
-            data: {
-                user: {
-                    email: 'qwe123@gmail.com',
-                    password: '123'
-                }
-            }
-        }) .then(res => {
-            console.log('success', res)
-            setIsSubmitting(false)
-        }) .catch(error => {
-            console.log('err', error)
-            setIsSubmitting(false)
-        })
+      }
     })
+    console.log('values', email, password)
+  }
 
-    return (
-        <div className='auth-page'>
-
-          <div className="container page">
-              <div className="row">
-                  <div className="col-md-6 offset-md-3 col-xs-12">
-                      <h1 className='text-xs-center'>Login</h1>
-                      <p className='text-xs-center'>
-                          <Link to='register'>Need an account?</Link>
-                      </p>
-                      <form onSubmit={handleSubmit}>
-                          <fieldset>
-                              <fieldset className="form-group">
-                                  <input
-                                      type='email'
-                                      className='form-control form-control-lg'
-                                      placeholder='Email'
-                                      value={email}
-                                      onChange={e => setEmail(e.target.value)}
-                                  />
-                              </fieldset>
-
-                              <fieldset className="form-group">
-                                  <input
-                                      type='password'
-                                      className='form-control form-control-lg'
-                                      placeholder='Password'
-                                      value={password}
-                                      onChange={e => setPassword(e.target.value)}
-                                  />
-                              </fieldset>
-
-                              <button className='btn btn-lg btn-primary pull-xs-right' type='submit' disabled={isSubmitting}>
-                                  Sign In
-                              </button>
-
-                          </fieldset>
-                      </form>
-                  </div>
-              </div>
+  return (
+    <div className="auth-page">
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-6 offset-md-3 col-xs-12">
+            <h1 className="text-xs-center">Sign in</h1>
+            <p className="text-xs-center">
+              <Link to="/register">Need an account?</Link>
+            </p>
+            <form onSubmit={handleSubmit}>
+              <fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                  />
+                </fieldset>
+                <button
+                  disabled={isLoading}
+                  className="btn btn-lg btn-primary pull-xs-right"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+              </fieldset>
+            </form>
           </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Authentication
